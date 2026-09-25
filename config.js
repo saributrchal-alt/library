d340a846001c8fcc98cb071d1019728d0ddc65e2
saributrchal-alt <saributrchal@gmail.com>
2026-09-25 22:28:17 +0700

    Add library config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// Set these public values after the book catalog has been provisioned.
+// The Supabase anon key is public; never place a service-role key in this file.
+window.LIBRARY_CONFIG = {
+  supabaseUrl: '',
+  supabaseAnonKey: '',
+  templeMemberUrl: 'https://watt.nathoeng.com/'
+};
